371ae72d51ab85e06ca31a049ae7be2a52e14b26
SangwooRyu <mrryu006@naver.com>
2020-09-21 18:24:01 +0900

    fix typo

--- modules/plog/plog.js
+++ modules/plog/plog.js
@@ -92,8 +92,8 @@ export default class ParticipantLog {
                 if(!this.logIdentity[idFromPacket]){
                     this.logIdentity[idFromPacket] = message[userId];
                 }
-                else {
-                    if(this.time_convert(message[userId]["joinTime"]) < this.logIdentity[idFromPacket]["joinTime"]){ //incoming is older
+                if (this.logIdentity[idFromPacket]) {
+                    if(this.time_convert(message[userId]["joinTime"]) < this.time_convert(this.logIdentity[idFromPacket]["joinTime"])){ //incoming is older
                         this.logIdentity[idFromPacket]["joinTime"] = message[userId]["joinTime"];
                     }
                     else { //incoming is newer
